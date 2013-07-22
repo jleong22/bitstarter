205856c7a82f04f7616d2d0eefd434809f5127f4
Jeff Leong <jeff.leong22@yahoo.com>
2013-07-22 22:31:09 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -2,12 +2,7 @@ var express = require('express');
 var app = express();
 app.use(express.logger());
 
-fs.readFileSync('bitstarter/index.html', function (err, data){
-    if (err) throw err;
-    console.log(data);
-]);
-
-buf.toString('utf8');
+fs.readFileSync('bitstarter/index.html', 'utf8');
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
