962eef469a3ba0bb6c2e441e9deb5d15491d305a
Jeff Leong <jeff.leong22@yahoo.com>
2013-07-22 22:26:29 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -2,9 +2,12 @@ var express = require('express');
 var app = express();
 app.use(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World 2!');
-});
+fs.readFileSync('bitstarter/index.html', function (err, data){
+    if (err) throw err;
+    console.log(data);
+]);
+
+buf.toString('utf8');
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
